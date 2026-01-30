045f63ff8feb687898508c3c6eeaeeba0b20e65d
Levi Alviter <levi.olivalviter@gmail.com>
2026-01-29 20:27:25 -0600

    First commit

---
+++ networks-starting-setup/app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios').default;
+const mongoose = require('mongoose');
+
+const Favorite = require('./models/favorite');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/favorites', async (req, res) => {
+  const favorites = await Favorite.find();
+  res.status(200).json({
+    favorites: favorites,
+  });
+});
+
+app.post('/favorites', async (req, res) => {
+  const favName = req.body.name;
+  const favType = req.body.type;
+  const favUrl = req.body.url;
+
+  try {
+    if (favType !== 'movie' && favType !== 'character') {
+      throw new Error('"type" should be "movie" or "character"!');
+    }
+    const existingFav = await Favorite.findOne({ name: favName });
+    if (existingFav) {
+      throw new Error('Favorite exists already!');
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  const favorite = new Favorite({
+    name: favName,
+    type: favType,
+    url: favUrl,
+  });
+
+  try {
+    await favorite.save();
+    res
+      .status(201)
+      .json({ message: 'Favorite saved!', favorite: favorite.toObject() });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+});
+
+app.get('/movies', async (_, res) => {
+  try {
+    const response = await axios.get('https://swapi.dev/api/films');
+    res.status(200).json({ movies: response.data });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+});
+
+app.get('/people', async (_, res) => {
+  try {
+    const response = await axios.get('https://swapi.dev/api/people');
+    res.status(200).json({ people: response.data });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+});
+
+mongoose.connect(  
+  // 'mongodb://localhost:27017/swfavorites',
+  // ! FOR LOCAL MACHINE SERVICE
+  // ! if you have the mongodb server running in your local machine you have to tweak the url
+  // ! to the next one
+  // ? 'mongodb://host.docker.internal:27017/swfavorites',
+  // ! host.docker.internal is recognized by docker and translated to the IP address of the host machine
+  // ! doing the rest for you!
+  // ! it can be also http://host.docker.internal
+
+  // ? to connect to mongo db container
+  // ? you need the IP address, look for the ip address of the container's service
+  // ? using docker container inspect container-name 
+  // ? look for the "NetworkSettings" and find the port and IPAddress
+  // 'mongodb://172.17.0.2:27017/swfavorites',
+
+  // ? to connect to mongo db container by docker docker
+  // ? 'mongodb://container-name:27017/swfavorites',
+  'mongodb://mongodb:27017/swfavorites',
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(3000);
+    }
+  }
+);
